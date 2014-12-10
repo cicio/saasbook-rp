aeb608bdf462b17f0cd82660a0b9074c4a8e9edc
cicio <m@navegawireless.com>
2014-12-10 16:05:27 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = '88d4ab183594794f880a6718cb35feeca230314308d80372b3acefb4121a8af8b5a367c3c565c10622b639cce6949b44c07299f00c8969bfe3780648d547aac3'
