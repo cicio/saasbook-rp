b0113d612cfdf1b24af4537515e70f51a8506940
cicio <m@navegawireless.com>
2014-12-10 21:40:19 -0400

    added actions edit, update, destroy, created edit view and modified show view to add delete link. End of the basic Chapter 4.exercise

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,5 +19,26 @@ class MoviesController < ApplicationController
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
+  end  
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated"
+    respond_to do |client_wants|
+      client_wants.html {  redirect_to movie_path(@movie)  }
+      client_wants.xml  {  render :xml => @movie.to_xml    }  
+    end
+  end
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice]= "#{@movie.title} was deleted!"
+    redirect_to movies_path
   end
 end
+
